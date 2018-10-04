a41bf54686ebff60721a67238af35f7a0563d61d
Lise Brackbill <menagerie@hevanet.com>
2018-10-03 20:42:58 -0700

    playing with input code

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('hello bcpreppers!');
+var countTo = prompt('how high shall we count even numbers')
+
+var sure = confirm('are you sure you want to count to ' + countTo)
+
+if (sure) {
+for (var i = 0; i < countTo; i ++) {
+    var remainder = i % 2;
+    if (remainder === 0) {
+        console.log (i, 'is even');
+    }
+}
+}
